51730a6e73f8fa879d5569b98f4f896ac735e7e3
Gábor Vass <hundeva@gmail.com>
2020-06-18 22:13:00 +0530

    Add option to change icon size
    Signed-off-by: Zaviyar786 <beingme639@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -141,6 +141,7 @@ public final class Utilities {
     public static final String KEY_NOTIFICATION_GESTURE = "pref_notification_gesture";
 
     private static final long WAIT_BEFORE_RESTART = 250;
+    public static final String ICON_SIZE = "pref_icon_size";
 
     /**
      * Indicates if the device has a debug build. Should only be used to store additional info or
@@ -790,5 +791,32 @@ public final class Utilities {
     public static boolean hasSecureKeyguard(Context context) {
         final KeyguardManager keyguardManager = context.getSystemService(KeyguardManager.class);
         return keyguardManager != null && keyguardManager.isKeyguardSecure();
+
+}
+
+    public static float getIconSizeModifier(Context context) {
+        String saved = getPrefs(context).getString(ICON_SIZE, "average");
+        float offset;
+        switch (saved) {
+            case "extrasmall":
+                offset = 0.75F;
+                break;
+            case "small":
+                offset = 0.90F;
+                break;
+            case "average":
+                offset = 1.00F;
+                break;
+            case "large":
+                offset = 1.10F;
+                break;
+            case "extralarge":
+                offset = 1.25F;
+                break;
+            default:
+                offset = 1.00F;
+                break;
+        }
+        return offset;
     }
 }
